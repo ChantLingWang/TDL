3cab5d7d1ac78be055834ef34869ed3acd4ffb7b
LianLingHao <liaolinghao@simplylab.xyz>
2025-07-16 22:46:12 +0800

    优化数据库管理类，扩展其构造函数属性，添加一些用于笔记的注释，新增连接池配置和ssl凭证配置。

--- services/mongodb_service.py
+++ services/mongodb_service.py
@@ -1,5 +1,8 @@
 import asyncio
-from motor.motor_asyncio import AsyncIOMotorClient
+import ssl
+from typing import Optional
+from datetime import datetime
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from pymongo.errors import ConnectionFailure
 import logging
 
@@ -8,17 +11,54 @@ logging.basicConfig(level=logging.INFO)#这是python的日志配置，其中leve
 logger = logging.getLogger(__name__)#创建日志记录器，使用logging.getLogger()方法，__name__表示为当前文件的name，如这里就是mongodb_service
 
 
-class MongoDBService:
+class MongoDBServiceManager:
     """数据库连接管理类"""
     def __init__(
-        self,connection_string:str="mongodb://localhost:27017",    #这里的链接应该为变量，在生产端时，要使用生产端数据库链接
-        database_name:str="TDL"
+        self,
+        connection_string:str="mongodb://localhost:27017",    #这里的链接应该为变量，在生产端时，要使用生产端数据库链接
+        database_name:str="TDL_local_test_database",
+        max_pool_size: int=10,
+        min_pool_size: int=1,
+        max_idle_time_ms: int=30000,
+        server_selection_timeout_ms: int=5000,
+        connect_timeout_ms: int=10000,
+        socket_timeout_ms: int=10000,
+        retry_writes: bool = True,
+        retry_reads: bool = True,
+        use_ssl: bool = False,
+        ssl_cert_reqs: int = ssl.CERT_NONE,
     ):
+        #细看代码，将我们定义的两个字段赋值给我们self属性
         self.connection_string = connection_string
         self.database_name = database_name
-        self.client = None
-        self.database = None
-        self.is_connected = False
+
+        #下面这段是连接状态的一些属性
+        # 其实下面这句代码等于 self.client: Union[AsyncIOMotorClient, None] = None，意思是这个字段可以是AsyncIOMotorClient也可以是None，但默认是None
+        self.client : Optional[AsyncIOMotorClient] = None  #默认值写在等号后，这部分要注意到optional这个关键字，是一个范围关键字，这个关键字表示这些属性可以是指定类型或者None
+        self.database : Optional[AsyncIOMotorDatabase] = None
+        self.is_connected: bool = False
+        self.connection_timeout_ms: Optional[datetime] = None
+
+        #这里是连接池的配置，python是动态语言，可以在构造函数直接定义属性，比如connection_config，这是个字典属性，由动态定义直接定义出来的
+        self.connection_config = {
+            'maxPoolSize': max_pool_size,
+            'minPoolSize': min_pool_size,
+            'maxIdleTimeMS': max_idle_time_ms,
+            'serverSelectionTimeoutMS': server_selection_timeout_ms,
+            'connectTimeoutMS': connect_timeout_ms,
+            'socketTimeoutMS': socket_timeout_ms,
+            'retryWrites': retry_writes,
+            'retryReads': retry_reads
+        }
+
+        #这是ssl凭证配置，当ssl为True时，我们就利用update方法对connection_config这个字典属性进行扩展   ->  字典有很多功能强大的内置方法，在阅读代码时需要注意
+        if use_ssl:
+            self.connection_config.update({
+                'ssl':True,
+                'ssl_cert_reqs': ssl_cert_reqs
+            })
+
+        logger.info(f"MongoDB 连接管理器初始化完成 - 数据库: {database_name}")
 
     async def connect(self):
         try:
