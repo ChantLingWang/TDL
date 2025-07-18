03f4835f2732b77eafdbf82b976dd544ab6b43dc
LianLingHao <liaolinghao@simplylab.xyz>
2025-07-18 17:30:27 +0800

    优化

--- api_services/api.py
+++ api_services/api.py
@@ -1,6 +1,9 @@
+import asyncio
+
 import uvicorn
 from fastapi import FastAPI
 from requests import Response, Request
+from services.mongodb_service import main
 
 app = FastAPI()
 
@@ -11,6 +14,9 @@ async def root():
 
 
 if __name__ == "__main__":
+    #在api启动时，就自动连接到数据库
+    asyncio.run(main())
+
     uvicorn.run(
         app="api:app",
         host="127.0.0.1",
