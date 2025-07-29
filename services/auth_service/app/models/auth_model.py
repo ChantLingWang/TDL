65f7b0bf63a84003f7b5a9101ada8d450f68782c
LianLingHao <liaolinghao@simplylab.xyz>
2025-07-29 15:23:27 +0800

    修复redis返回验证码的格式bug，新增两个核销验证码登陆和注册的接口。

--- services/auth_service/app/models/auth_model.py
+++ services/auth_service/app/models/auth_model.py
@@ -14,5 +14,11 @@ class SendCodeRequest(BaseModel):
     email: EmailStr = Field(..., description="用户邮箱")
     
 class VerifyCodeRequest(BaseModel):
+    username: str = Field(..., min_length=3, max_length=10,description="用户名")
+    email: EmailStr = Field(..., description="用户邮箱")
+    password: str = Field(..., description="用户密码")
+    code: str = Field(..., min_length=6, max_length=6,description="验证码")
+
+class VerifyCodeLoginRequest(BaseModel):
     email: EmailStr = Field(..., description="用户邮箱")
     code: str = Field(..., min_length=6, max_length=6,description="验证码")
